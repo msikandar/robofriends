8ac7390a2daac5e391a0c69860574055104e11cc
Ubuntu <ubuntu@ip-172-31-27-108.ap-southeast-1.compute.internal>
2019-04-28 01:28:31 +0000

    initial

---
+++ src/CardList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Card from './Card';
+
+
+function CardList({robots}) {
+    const cardComponent = robots.map((user,i) => {
+    
+    return (
+        <Card id={robots[i].id} name={robots[i].name} email={robots[i].email}/>
+        );
+    });
+        return(
+            <div>
+            {cardComponent}
+            </div>
+            );
+}
+
+export default CardList;
---
+++ src/robots.js
@@ -0,0 +1,115 @@
+export const robots=[
+    {
+    id:1,
+    name:'Leanne Graham',
+    username:'Bret',
+    email:'Sincere@april.biz'
+  },
+  {
+     id:2,
+     name:'Bruce Lee',
+     username:'Ok',
+     email:'master@may.in'
+  },
+  {
+      id:3,
+      name:'Under Taker',
+      username:'Rock',
+      email:'only@may.uk'
+},
+{
+      id:4,
+      name:'Leanne Graham',
+      username:'Bret',
+      email:'Serious@may.org'
+},
+{
+      id:5,
+      name:'Leanne Graham',
+      username:'Bret',
+      email:'Serious@may.org'
+},
+{
+      id:6,
+      name:'Leanne Graham',
+      username:'Bret',
+      email:'Serious@may.org'
+},
+{
+      id:7,
+      name:'Leanne Graham',
+      username:'Bret',
+      email:'Serious@may.org'
+},
+{
+      id:8,
+      name:'Leanne Graham',
+      username:'Bret',
+      email:'Serious@may.org'
+},
+{
+      id:9,
+      name:'Leanne Graham',
+      username:'Bret',
+      email:'Serious@may.org'
+},
+{
+      id:10,
+      name:'Leanne Graham',
+      username:'Bret',
+      email:'Serious@may.org'
+},
+{
+      id:11,
+      name:'Leanne Graham',
+      username:'Bret',
+      email:'Serious@may.org'
+},
+{
+      id:12,
+      name:'Leanne Graham',
+      username:'Bret',
+      email:'Serious@may.org'
+},
+{
+      id:13,
+      name:'Leanne Graham',
+      username:'Bret',
+      email:'Serious@may.org'
+},{
+      id:14,
+      name:'Leanne Graham',
+      username:'Bret',
+      email:'Serious@may.org'
+},
+{
+      id:15,
+      name:'Leanne Graham',
+      username:'Bret',
+      email:'Serious@may.org'
+},
+{
+      id:16,
+      name:'Leanne Graham',
+      username:'Bret',
+      email:'Serious@may.org'
+},
+{
+      id:17,
+      name:'Leanne Graham',
+      username:'Bret',
+      email:'Serious@may.org'
+},
+{
+      id:18,
+      name:'Leanne Graham',
+      username:'Bret',
+      email:'Serious@may.org'
+},
+{
+      id:19,
+      name:'Leanne Graham',
+      username:'Bret',
+      email:'Serious@may.org'
+}
+]
